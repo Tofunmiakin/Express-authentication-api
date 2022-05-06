7285b6637cb8921a772af51eb59f2f0f5352127b
R04DM4N <58348842+Tofunmiakin@users.noreply.github.com>
2022-05-06 16:19:53 +0100

    correct port

--- api/server.js
+++ api/server.js
@@ -56,7 +56,7 @@ app.use(passport.session());
 //ROUTES
 app.use('/auth', AuthRoute);
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
